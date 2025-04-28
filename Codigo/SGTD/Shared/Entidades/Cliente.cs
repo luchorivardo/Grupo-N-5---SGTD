fefb3b330a59cc68bfc9214485b78115f83257e3
luchorivardo <128877604+luchorivardo@users.noreply.github.com>
2025-04-28 20:20:31 -0300

    add/data

--- Codigo/SGTD/Shared/Entidades/Cliente.cs
+++ Codigo/SGTD/Shared/Entidades/Cliente.cs
@@ -8,10 +8,12 @@ namespace Shared.Entidades
 {
     public class Cliente : EntidadBase
     {
+        
         public string Nombre { get; set; }
         public int Dni { get; set; }
         public string Telefono { get; set; }
         public string Direccion { get; set; }
+        public int CiudadId { get; set; }
         public Ciudad Ciudad { get; set; }
 
     }
