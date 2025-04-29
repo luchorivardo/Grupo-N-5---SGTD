dc083835edf01de0ad6bbd33ed46701e8cc2a023
FrancoCarlosAudagnotti <166541585+FrancoCarlosAudagnotti@users.noreply.github.com>
2025-04-29 10:30:54 -0300

    DTOs Creados
    Agregué la carpeta DTOs con las subcarpetas 'entidad x'DTOs con los DTOs de Creacion(Create), Lectura(Read) y Actializar(Update) respectivos de cada entidad.

---
+++ Codigo/SGTD/Shared/DTOs/ClienteDTOs/ClienteReadDTO.cs
@@ -0,0 +1,19 @@
+﻿using Shared.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs.ClienteDTOs
+{
+    public class ClienteReadDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Dni { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public int CiudadId { get; set; }
+    }
+}
