450f6247c7d1f926f97afa253ef3c9ea4ef27a90
FrancoCarlosAudagnotti <166541585+FrancoCarlosAudagnotti@users.noreply.github.com>
2025-05-22 14:26:49 -0300

    RolService creado
    Cree los métodos y el mapper, basicamente es lo mismo que estado pero con otros nombres

---
+++ Codigo/SGTD/Service/Mappers/RolMapper.cs
@@ -0,0 +1,15 @@
+﻿using Riok.Mapperly.Abstractions;
+using Shared.DTOs.RolDTOs;
+using Shared.Entidades;
+
+namespace Service.Mappers
+{
+    [Mapper]
+    public partial class RolMapper
+    {
+        public partial Rol ToEntity(RolCreateDTO dto);
+        public partial RolReadDTO ToReadDto(Rol entity);
+
+        public partial void UpdateEntity(RolUpdateDTO dto, Rol entity);
+    }
+}
--- Codigo/SGTD/Shared/DTOs/RolDTOs/RolUpdateDTO.cs
+++ Codigo/SGTD/Shared/DTOs/RolDTOs/RolUpdateDTO.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Shared.DTOs.RolDTOs
 {
-    internal class RolUpdateDTO
+    public class RolUpdateDTO
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
